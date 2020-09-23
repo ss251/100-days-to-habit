bd07f5e028c3c617f6df0d86fe409d0e5b98d7a4
ss251 <ss251@uw.edu>
2020-09-24 02:54:06 +0530

    changes now reflected in state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import Button from "./Button";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import base from "./base";
+import buttons from "./buttons";
 
 class App extends React.Component {
   state = {
     buttons: {},
+  };
+
+  componentDidMount() {
+    this.setState({ buttons });
+  }
+
+  clickButton = (key) => {
+    const buttons = { ...this.state.buttons };
+    buttons[`button${key}`].condition = !buttons[`button${key}`].condition;
+    this.setState({ buttons });
   };
 
   render() {
@@ -13,8 +24,17 @@ class App extends React.Component {
     for (var i = 1; i <= 100; i++) {
       array.push(i);
     }
+    //console.log(this.state.buttons[`button${100}`].condition);
     const buttonLoop = array.map((number) => {
-      return <Button key={number} index={number}></Button>;
+      return (
+        <Button
+          key={number}
+          index={number}
+          addButton={this.addButton}
+          clickButton={this.clickButton}
+          condition={buttons[`button${number}`].condition}
+        ></Button>
+      );
     });
     return (
       <html>
--- src/Button.js
+++ src/Button.js
@@ -1,23 +1,19 @@
 import React from "react";
 
 class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      condition: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState({
-      condition: !this.state.condition,
-    });
-  }
+  handleClick = () => {
+    //this.state.buttons. = !this.props.condition;
+    this.props.clickButton(this.props.index);
+  };
+  //this.setState({
+  //    condition: !this.state.condition,
+  //  });
+
   render() {
     return (
       <div
         onClick={this.handleClick}
-        className={this.state.condition ? "button-toggled" : "button"}
+        className={this.props.condition ? "button-toggled" : "button"}
       >
         <button className={"button"}>{this.props.index}</button>
       </div>
